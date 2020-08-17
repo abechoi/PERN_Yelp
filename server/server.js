32f7913a71ec73b1789c2e74aca98aa165b3130d
Abraham Choi <abe@MBP01.local>
2020-08-17 16:34:07 -0700

    HomePage Layout 3:02:03

--- server/server.js
+++ server/server.js
@@ -41,8 +41,7 @@ app.get("/api/v1/restaurants", async (req, res) => {
         restaurants: results.rows
       }
     });
-  }
-  catch(err){console.log(err)}
+  }catch(err){console.log(err)}
 
 });
 // GET    | Retrieve one restaurant    | /api/v1/restaurants/:id
@@ -59,8 +58,7 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
         restaurant: results.rows[0]
       }
     });
-  }
-  catch(err){console.log(err)}
+  }catch(err){console.log(err)}
 
 });
 // POST   | Create restaurant         | /api/v1/restaurants
@@ -80,26 +78,48 @@ app.post("/api/v1/restaurants", async (req, res) => {
         restaurant: results.rows[0]
       }
     });
-  }
-  catch(err){console.log(err)}
+  }catch(err){console.log(err)}
 
 });
 // PUT    | Update restaurant         | /api/v1/restaurants/:id
-app.put("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params.id);
-  console.log(req.body);
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: "raising canes"
-    }
-  });
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+
+  try{
+
+    const results = await db.query("UPDATE restaurants SET name = $1, location=$2,price_range=$3 WHERE id=$4 returning *", [
+      req.body.name,
+      req.body.location,
+      req.body.price_range,
+      req.params.id,
+    ]);
+    console.log(req.params.id);
+    console.log(req.body);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+
+  }catch(err){console.log(err)}
+
 });
 // DELETE | Delete one restaurant     | /api/v1/restaurants/:id
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  res.status(204).json({
-    status: "success"
-  });
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  
+  try{
+
+    const results = await db.query("DELETE FROM restaurants WHERE id=$1", [req.params.id]);
+    res.status(204).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
+      }
+    });
+
+  }catch(err){console.log(err)};
+
 });
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
