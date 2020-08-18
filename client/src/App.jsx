90e7ebcf7b6bad0f83ec799dfaa7c74fbb404204
Abraham Choi <abe@MBP01.local>
2020-08-17 19:05:01 -0700

    Fetching Data 3:20:17

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import UpdatePage from './routes/UpdatePage';
 import RestaurantDetails from './routes/RestaurantDetails';
+import { RestaurantContextProvider } from './contexts/RestaurantContext';
 
 
 const App = () => {
-  return <div>
-    <Router>
-      <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
-      <Route exact path="/restaurants/:id" component={RestaurantDetails}/>
-      </Switch>
-    </Router>
-  </div>;
+  return (
+    <RestaurantContextProvider>
+      <div className="container">
+        <Router>
+          <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+          <Route exact path="/restaurants/:id" component={RestaurantDetails}/>
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantContextProvider>
+  );
 }
 export default App;
